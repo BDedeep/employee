b1de4803e3fce910bce9f557d230a8bcaed53bb0
BDedeep <155967594+BDedeep@users.noreply.github.com>
2025-04-18 21:59:39 +0530

    Update Employee.java

--- src/main/java/com/example/employee/model/Employee.java
+++ src/main/java/com/example/employee/model/Employee.java
@@ -1,17 +1,12 @@
 package com.example.employee.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
